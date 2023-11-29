11729561a918336c7350a1dba8130adde672ada5
Kimani Joseph <kimjose693@gmail.com>
2023-11-29 20:39:16 +0300

    Show musical

--- src/main/java/com/infinitops/musicaltickets/BookTicket.java
+++ src/main/java/com/infinitops/musicaltickets/BookTicket.java
@@ -1,16 +1,21 @@
 package com.infinitops.musicaltickets;
 
-import com.infinitops.musicaltickets.model.Schedule;
-import com.infinitops.musicaltickets.model.Venue;
-import com.infinitops.musicaltickets.model.Musical;
+import com.infinitops.musicaltickets.model.*;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+import org.controlsfx.control.Notifications;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -25,6 +30,8 @@ public class BookTicket  implements Initializable {
     public ComboBox<Venue> cbVenue;
     public ComboBox<String> cbTimeSlot;
     public ComboBox<String> cbSeatNumber;
+    public AnchorPane apParent;
+    public TableView<Ticket> tableTickets;
 
     @FXML
     private Label labelMusicalTitle;
@@ -37,6 +44,7 @@ public class BookTicket  implements Initializable {
 
     private Musical musical;
     private Schedule[] schedules;
+    List<Ticket> tickets = new ArrayList<>();
     public void setMusical(Musical musical){
         this.musical = musical;
         labelMusicalTitle.setText(musical.getTitle());
@@ -84,7 +92,98 @@ public class BookTicket  implements Initializable {
                 if(s.getVenue().equals(t1)) times.add(s.getDate()+ " " + s.getTime());
             }
             cbTimeSlot.setItems(FXCollections.observableList(times));
-            cbSeatNumber.setItems(FXCollections.observableList(Arrays.asList(t1.getSeats())));
+            if(t1 != null) cbSeatNumber.setItems(FXCollections.observableList(Arrays.asList(t1.getSeats())));
+        });
+        Platform.runLater(() -> {
+            tableTickets.prefWidthProperty().bind(apParent.widthProperty());
+            tableTickets.getColumns().removeAll(tableTickets.getColumns());
+            tableTickets.getColumns().addAll(createColumns(new MyTableColumn[]{
+                    new MyTableColumn("Musical ", "musicalName", 0.20),
+                    new MyTableColumn("Venue", "venueName", 0.20),
+                    new MyTableColumn("Time Slot", "timeSlot", 0.20),
+                    new MyTableColumn("Seat Number", "seatNumber", 0.18),
+                    new MyTableColumn("Remove Ticket", "removeTicketButton", 0.18),
+            }));
         });
     }
+
+    public void bookTicket(){
+        Venue v = cbVenue.getValue();
+        if (v == null){
+            createNotification(4, "Select the venue first");
+            return;
+        }
+        String ticketType = cbTicketType.getValue();
+        if(ticketType == null){
+            createNotification(4, "Select Ticket type first");
+            return;
+        }
+        String timeSlot = cbTimeSlot.getValue();
+        if (timeSlot == null){
+            createNotification(4, "Select time slot first");
+            return;
+        }
+        String seatNumber = cbSeatNumber.getValue();
+        if(seatNumber == null){
+            createNotification(4, "Select seat number");
+            return;
+        }
+        Ticket t = new Ticket();
+        t.setMusicalId(musical.get_id());
+        t.setMusicalName(musical.getTitle());
+        t.setSeatNumber(seatNumber);
+        t.setVenueId(v.get_id());
+        t.setVenueName(v.getName());
+        t.setTimeSlot(timeSlot);
+        t.setBookTicket(this);
+        tickets.add(t);
+
+        tableTickets.setItems(FXCollections.observableList(tickets));
+        cbVenue.getSelectionModel().clearSelection();
+        cbTicketType.getSelectionModel().clearSelection();
+        cbTimeSlot.getSelectionModel().clearSelection();
+        cbSeatNumber.getSelectionModel().clearSelection();
+    }
+
+    public void removeTicket(Ticket t){
+        tickets.remove(t);
+        tableTickets.setItems(FXCollections.observableList(tickets));
+    }
+
+    private void createNotification(int type, String message) {
+        Notifications n = Notifications.create()
+                .owner(apParent)
+                .text(message)
+                .hideAfter(Duration.seconds(8))
+                .darkStyle()
+                .position(Pos.BOTTOM_RIGHT);
+        Platform.runLater(() -> {
+            switch (type) {
+                case -1:
+                    n.showError();
+                    break;
+                case 0:
+                    n.showInformation();
+                    break;
+                default:
+                    n.showWarning();
+            }
+
+        });
+
+    }
+
+    private TableColumn[] createColumns(MyTableColumn[] myTableColumns) {
+        TableColumn[] tableColumns = new TableColumn[]{};
+        List<TableColumn<String, String>> tableColumnList = new ArrayList<>();
+        for (MyTableColumn m : myTableColumns) {
+            TableColumn<String, String> tableColumn = new TableColumn<>(m.getName());
+            tableColumn.setCellValueFactory(new PropertyValueFactory<>(m.getProperty()));
+            tableColumn.prefWidthProperty().bind(tableTickets.prefWidthProperty().multiply(m.getMultiplier()));
+            tableColumn.setResizable(true);
+            tableColumnList.add(tableColumn);
+        }
+        return tableColumnList.toArray(tableColumns);
+    }
+
 }
---
+++ src/main/java/com/infinitops/musicaltickets/model/Ticket.java
@@ -0,0 +1,83 @@
+package com.infinitops.musicaltickets.model;
+
+import com.infinitops.musicaltickets.BookTicket;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Paint;
+
+public class Ticket {
+    private String musicalName;
+    private int musicalId;
+    private String seatNumber;
+    private String venueName;
+    private int venueId;
+    private String timeSlot;
+    private BookTicket bookTicket;
+
+    public void setBookTicket(BookTicket bookTicket) {
+        this.bookTicket = bookTicket;
+    }
+
+    public String getMusicalName() {
+        return musicalName;
+    }
+
+    public void setMusicalName(String musicalName) {
+        this.musicalName = musicalName;
+    }
+
+    public int getMusicalId() {
+        return musicalId;
+    }
+
+    public void setMusicalId(int musicalId) {
+        this.musicalId = musicalId;
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public String getVenueName() {
+        return venueName;
+    }
+
+    public void setVenueName(String venueName) {
+        this.venueName = venueName;
+    }
+
+    public int getVenueId() {
+        return venueId;
+    }
+
+    public void setVenueId(int venueId) {
+        this.venueId = venueId;
+    }
+
+    public String getTimeSlot() {
+        return timeSlot;
+    }
+
+    public void setTimeSlot(String timeSlot) {
+        this.timeSlot = timeSlot;
+    }
+
+
+
+    public Button getRemoveTicketButton(){
+        Button btnRemoveTicket = new Button();
+        btnRemoveTicket.setText("Remove");
+//        FontAwesomeIconView icon = new FontAwesomeIconView(FontAwesomeIcon.EXCHANGE);
+//        icon.setFill(Paint.valueOf("#19D019"));
+//        icon.setSize("12.0");
+        btnRemoveTicket.setStyle("-fx-background-color: red;");
+//        btnRemoveTicket.setGraphic(icon);
+        btnRemoveTicket.setOnAction(event -> {
+            if (bookTicket != null) bookTicket.removeTicket(this);
+        });
+        return btnRemoveTicket;
+    }
+}
